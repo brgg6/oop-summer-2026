0563d119fa60bb4a4c10c2aa1d3603b15cf44318
brgg6 <mmcamel12@gmail.com>
2026-03-12 09:53:28 +0000

    lecture 2 entry test

---
+++ lecture_2_entry_test.py
@@ -0,0 +1,19 @@
+# Integer variable with day of birth
+my_birthday = 16
+print(my_birthday)
+print(type(my_birthday))
+
+# String variable with full name
+full_name = "Furkan"
+print(full_name)
+print(type(full_name))
+
+# Float variable for last semester grade
+last_semester_grade = 3.5
+
+# Convert float to integer
+converted_grade = int(last_semester_grade)
+
+# Display value and type of converted value
+print(converted_grade)
+print(type(converted_grade))
